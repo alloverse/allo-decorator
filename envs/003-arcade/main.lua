c960a6408b2626d5918c536c6e68dda10ecca1d5
Nevyn Bengtsson <nevyn@alloverse.com>
2022-02-03 11:36:40 +0100

    background sounds for the arcade

--- envs/003-arcade/main.lua
+++ envs/003-arcade/main.lua
@@ -17,6 +17,12 @@ local assets = {
     {name = "houseplant03.glb", hasTransparency = false, pos={4.5, 0.1, 2.5}},
     {name = "houseplant03.glb", hasTransparency = false, pos={4.5, 0.1, -2.5}, rot={3.14/3, 0,1,0}}, 
 
+    {name = "sounds/416489__tennysonmusic__ghostly-japanese-game-center-left.mp3", volume = 0.3, pos={2.5, 0.1, 4.5}},
+    {name = "sounds/416489__tennysonmusic__ghostly-japanese-game-center-right.mp3", volume = 0.3, pos={2.5, 0.1, -4.5}},
+
+    {name = "sounds/460119__nickmaysoundmusic__air-hockey-arcade-ambience-distant-music-left.mp3", volume = 0.5, pos={-2.5, 0.1, 4.5}},
+    {name = "sounds/460119__nickmaysoundmusic__air-hockey-arcade-ambience-distant-music-right.mp3", volume = 0.5, pos={-2.5, 0.1, -4.5}},
+
 
     --{name = "NES.glb", hasTransparency = false,         pos={-4, 1, 0}},
     -- {name = "SNES.glb", hasTransparency = false,        pos={0, 0.1, 1}},
@@ -29,6 +35,7 @@ function ArcadeEnv.load()
     if not root then
         root = ui.View()
         loadEnvAssets(root, assets)
+
         root.bounds.pose
             :rotate(-3.14/2, 0,1,0)
             :move(0, 0.01, -6.3)
--- lua/env.lua
+++ lua/env.lua
@@ -1,14 +1,17 @@
 function loadEnvAssets(root, assets)
-    print("Loading the assets for env root", root)
-    -- Create assets and views for each
-    for _, spec in ipairs(assets) do
+  print("Loading the assets for env root", root)
+  -- Create assets and views for each
+  for _, spec in ipairs(assets) do
 
-      print("Drawing asset:", spec.name)
+    print("Drawing asset:", spec.name)
 
-      local asset = ui.Asset.File(assets.path..spec.name)
-      app.assetManager:add(asset, true)
+    local asset = ui.Asset.File(assets.path..spec.name)
+    app.assetManager:add(asset, true)
 
-      local view = ui.ModelView(nil, asset)
+    local view = nil
+    if spec.name:match("glb$") then
+    
+      view = ui.ModelView(nil, asset)
       view.hasTransparency = spec.hasTransparency
       view.customSpecAttributes = {
           material = {
@@ -19,13 +22,20 @@ function loadEnvAssets(root, assets)
       if spec.backfaceCulling then
           view.customSpecAttributes.material.backfaceCulling = true
       end
-      if spec.rot then
-          view.bounds:rotate(unpack(spec.rot))
+    elseif spec.name:match("mp3$") then
+      view = ui.Speaker(nil, asset)
+      if spec.volume then
+        view.volume = spec.volume
       end
-      if spec.pos then
-          view.bounds:move(unpack(spec.pos))
-      end
-
-      root:addSubview(view)
     end
+
+    if spec.rot then
+        view.bounds:rotate(unpack(spec.rot))
+    end
+    if spec.pos then
+        view.bounds:move(unpack(spec.pos))
+    end
+
+    root:addSubview(view)
+  end
 end
