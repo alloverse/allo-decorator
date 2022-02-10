72516a8fa133d6747cc1ff3841941c1280ed5b8c
Tobias Kask <tobias.kask@gmail.com>
2022-02-10 12:08:50 +0100

    textured red carpet, gold saucer vibes, moving root so user spawn facing the entrance

--- envs/003-arcade/main.lua
+++ envs/003-arcade/main.lua
@@ -17,11 +17,14 @@ local assets = {
     {name = "houseplant03.glb", hasTransparency = false, pos={4.5, 0.1, 2.5}},
     {name = "houseplant03.glb", hasTransparency = false, pos={4.5, 0.1, -2.5}, rot={3.14/3, 0,1,0}}, 
 
-    {name = "sounds/416489__tennysonmusic__ghostly-japanese-game-center-left.mp3", volume = 0.3, pos={2.5, 0.1, 4.5}},
+    {name = "sounds/416489__tennysonmusic__ghostly-japanese-game-center-left.mp3", volume = 0.3, pos={-2.5, 0.1, 4.5}},
     {name = "sounds/416489__tennysonmusic__ghostly-japanese-game-center-right.mp3", volume = 0.3, pos={2.5, 0.1, -4.5}},
 
     {name = "sounds/460119__nickmaysoundmusic__air-hockey-arcade-ambience-distant-music-left.mp3", volume = 0.5, pos={-2.5, 0.1, 4.5}},
     {name = "sounds/460119__nickmaysoundmusic__air-hockey-arcade-ambience-distant-music-right.mp3", volume = 0.5, pos={-2.5, 0.1, -4.5}},
+
+    {name = "sounds/gold-saucer-left.mp3", volume = 0.5, pos={-2.5, 0.1, 4.5}},
+    {name = "sounds/gold-saucer-right.mp3", volume = 0.5, pos={-2.5, 0.1, -4.5}},
 }
 
 local root = nil
@@ -31,22 +34,21 @@ function ArcadeEnv.load()
         root = ui.View()
         loadEnvAssets(root, assets)
 
+        -- Add a red carpet leading to the dome
+        local carpetTextureAsset = ui.Asset.File("envs/003-arcade/assets/textures/red-carpet-tiled.png")
+        app.assetManager:add(carpetTextureAsset, true)
+
+        local redCarpet = ui.Surface(ui.Bounds( {size=ui.Size(10, 3.6, 0.001), pose=ui.Pose(10,0.1,0) }):rotate(-3.14/2, 1, 0, 0))
+        redCarpet:setColor({0.8, 0, 0, 1})
+        redCarpet:setTexture(carpetTextureAsset)
+        root:addSubview(redCarpet)
+        
+        
         root.bounds.pose
             :rotate(-3.14/2, 0,1,0)
-            :move(0, 0.01, -6.3)
+            :move(0, 0.01, -12)
     end
     app:addRootView(root)
-
-    -- Add a red carpet leading to the dome
-    local redCarpet = ui.Surface(ui.Bounds( {size=ui.Size(10, 2, 0.001), pose=ui.Pose(10,0,0) }):rotate(-3.14/2, 1, 0, 0))
-    local carpetTextureAsset = ui.Asset.File("envs/003-arcade/assets/textures/red-carpet-tiled.png")
-
-    print("asset:", carpetTextureAsset)
-
-    redCarpet:setColor({0.8, 0, 0, 1})
-    redCarpet:setTexture(carpetTextureAsset)
-
-    root:addSubview(redCarpet)
 
 end
 function ArcadeEnv.unload()
