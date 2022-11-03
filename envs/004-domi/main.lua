ac77c27abed1a1fb2800d7b1a9720f5d44244307
Nevyn Bengtsson <nevyn@alloverse.com>
2022-11-03 11:12:56 +0100

    add apps to meeting dome

--- envs/004-domi/main.lua
+++ envs/004-domi/main.lua
@@ -29,6 +29,7 @@ function load(root, assets)
 end
 
 local root = nil
+local apps = {}
 
 function DomiEnv.load()
     if not root then
@@ -36,9 +37,34 @@ function DomiEnv.load()
         load(root, assets)
         root.bounds.pose:move(0, 0.01, 0)
     end
+
+    loadEnvApps(apps, {
+        {
+            url= "alloapp:http://localhost:8000/allo-fileviewer",
+            pose=ui.Pose():rotate(3.14*0.15, 0,1,0):move(-2.0, 2, -3.0),
+            args= {}
+        },
+        {
+            url= "alloapp:http://localhost:8000/allo-fileviewer",
+            pose=ui.Pose():rotate(-3.14*0.05, 0,1,0):move(2.0, 2, -3.5),
+            args= {}
+        },
+        {
+            url= "alloapp:http://localhost:8000/allo-whiteboard",
+            pose=ui.Pose():rotate(-3.14*0.25, 0,1,0):move(4.8, 2, -1.5),
+            args= {}
+        },
+        {
+            url= "alloapp:http://localhost:8000/allo-todo",
+            pose=ui.Pose():rotate(-3.14*0.5, 0,1,0):move(5.5, 1.3, 1.0),
+            args= {}
+        },
+    })
+
     app:addRootView(root)
 end
 function DomiEnv.unload()
+    unloadEnvApps(apps)
     root:removeFromSuperview()
 end
 
